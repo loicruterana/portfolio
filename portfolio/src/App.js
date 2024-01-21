47afdf53db582d314c5b56584cec8cf6879f598a
loicruterana <lruterana@gmail.com>
2024-01-21 18:27:15 +0100

    test

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -26,7 +26,7 @@ function App() {
           <p>Passionné de cinéma, de code et de design, j'aime mélanger ces mondes pour créer des univers originaux</p>
           </div>
           <div class= 'about'>
-          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> */}
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> */}
           </div>
       </div>
       <div class="line">
