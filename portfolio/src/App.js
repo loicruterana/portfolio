9a3ef5921679e3c82ffca7615f10c885acb00a26
loicruterana <lruterana@gmail.com>
2024-01-21 18:24:04 +0100

    test

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -23,7 +23,7 @@ function App() {
             {/* <img class= 'flipx' src ='/images/titlz.png' alt='title' /> */}
           </div>
           <div class= 'about'>
-          <p>Passionné de cinéma, de code et de design, j'aime mélanger ces univers pour créer des univers originaux</p>
+          <p>Passionné de cinéma, de code et de design, j'aime mélanger ces mondes pour créer des univers originaux</p>
           </div>
           <div class= 'about'>
           {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> */}
